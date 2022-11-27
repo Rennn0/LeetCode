31c7bd86f7ccc70962a15732205285038647ad61
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-11-27 09:42:49 +0000

    0ms

--- arranging_coins.cpp
+++ arranging_coins.cpp
@@ -1,3 +1,8 @@
+// You have n coins and you want to build a staircase with these coins.
+//  The staircase consists of k rows where the ith row has exactly i coins. 
+//  The last row of the staircase may be incomplete.
+// Given the integer n, return the number of complete rows of the staircase you will build.
+
 
 int arrangeCoins(int n) {
 		long long int i=1;
